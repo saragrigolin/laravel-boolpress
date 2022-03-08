eaed7472e728541de3981f94983609a510830f56
saragrigolin <sara.grigolin95@gmail.com>
2022-03-08 17:17:17 +0100

    axios and paginate

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -8,7 +8,6 @@
                 <div class="card-body">
                     <h5 class="card-title">{{ post.title }}</h5>
                     <p class="card-text">{{ post.content }}</p>
-                    <p class="card-text">{{ post.category_id }}</p>
                 </div>
                 </div>
             </div>
