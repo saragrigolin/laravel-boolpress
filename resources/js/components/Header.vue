e32af4c9d6b7c88d87b7624b6ed10faac9daa048
saragrigolin <sara.grigolin95@gmail.com>
2022-03-08 17:17:05 +0100

    axios and paginate

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,13 +1,70 @@
 <template>
-    <div>Hello world - Header</div>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img id="logo" class="w-5" :src="logo" alt="logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item"  
+                    v-for="(item) in menuItems"
+                    :key="item.id"
+                    >
+                    <a class="nav-link" :href="item.link">{{ item.label }}</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
     export default {
         name: "Header",
+        data() {
+            return {
+                logo: require('../../img/WordPress_blue_logo.svg.png'),
+                menuItems: [
+                    {
+                        label: 'Item 1',
+                        link: '#',
+                        id: 1,
+                    },
+                    {
+                        label: 'Item 2',
+                        link: '#',
+                        id: 2,
+                    },
+                    {
+                        label: 'Item 3',
+                        link: '#',
+                        id: 3
+                    },
+                    {
+                        label: 'Item 4',
+                        link: '#',
+                        id: 4,
+                    },
+                    {
+                        label: 'Item 5',
+                        link: '#',
+                        id: 5,
+                    }
+                ],
+            }
+        },
     }
     </script>
 
     <style lang="scss">
 
+        .w-5 {
+            width: 5%;
+        }
     </style>
--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,14 +1,67 @@
 <template>
-    <div>Hello world - Main</div>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+            <div class="col" v-for="(post, index) in posts" :key="index">
+                <div class="card">
+                <img v-if="post.image" :src="'/storage/'+post.image" class="card-img-top" :alt="post.title">
+                <img v-else src="/storage/uploads/default.png" class="card-img-top" :alt="post.title">
+                <div class="card-body">
+                    <h5 class="card-title">{{ post.title }}</h5>
+                    <p class="card-text">{{ post.content }}</p>
+                    <p class="card-text">{{ post.category_id }}</p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-3 bg-light">
+            <ul class="list-inline bg-light d-flex justify-content-center">
+                <li class="list-inline-item"> <button v-if="prev_page_url" class="btn btn-primary" @click="changePage('prev_page_url')">Prev</button></li>
+                <li class="list-inline-item"> <button v-if="next_page_url" class="btn btn-primary" @click="changePage('next_page_url')">Next</button></li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
+import Axios from "axios"
+
 
     export default {
         name: "Main",
+        data() {
+            return {
+                posts: null,
+                next_page_url: null,
+                prev_page_url: null
+            }
+        },
+        created(){
+            this.getPosts('http://127.0.0.1:8001/api/posts');
+        },
+        methods: {
+            changePage(vs) {
+                let url = this[vs];
+                if(url) {
+                this.getPosts(url);
+                }
+            },
+            getPosts(url){
+                Axios.get(url).then(
+                    (result) => {
+                    this.posts = result.data.results.posts.data;
+                    this.next_page_url = result.data.results.posts.next_page_url;
+                    this.prev_page_url = result.data.results.posts.prev_page_url;
+                    });
+            }
+            
+            }
     }
     </script>
 
     <style lang="scss" scoped>
+
+        .card {
+            height: 100%;
+        }
 
     </style>
